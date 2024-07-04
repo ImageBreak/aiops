022be8f868502d2aa783a1776832bb1b45fe5583
perry <peilin.yuan@aipocloud.com>
2024-07-04 14:37:20 +0800

    use cosine

--- run.sh
+++ run.sh
@@ -21,7 +21,7 @@ git clone https://www.modelscope.cn/models/Xorbits/bge-m3.git demo/BAAI/bge-m3
 git clone https://www.modelscope.cn/models/Tolk8888/xiaobu-embedding-v2.git lier007/xiaobu-embedding-v2
 
 # download the reranker
-git clone https://www.modelscope.cn/models/mirror013/bge-reranker-v2-m3.git demo/BAAI/bge-reranker-v2-m3
+git clone https://www.modelscope.cn/models/AI-ModelScope/bge-reranker-v2-m3.git demo/BAAI/bge-reranker-v2-m3
 
 # download dataset
 git clone https://www.modelscope.cn/datasets/issaccv/aiops2024-challenge-dataset.git demo/dataset
